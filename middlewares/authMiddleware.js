ac023c14fb711ca100f908e6f500547bd085e9e3
BOBYwd <luismiguelsiri01032005@gmail.com>
2025-11-25 11:15:02 -0600

    Create authMiddleware.js

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export default function authMiddleware(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ error: "No autorizado" });
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token inválido" });
+  }
+}
